5fa377560603aec773cba0a4fea5ac644496387a
Ashlesha Singh <ashleshasingh00@gmail.com>
2026-03-24 20:57:37 +0530

    UC2

---
+++ UC2.java
@@ -0,0 +1,22 @@
+/**
+ * UC2 - Render OOPS as Banner using Print Statements
+ * 
+ * This program prints "OOPS" as a banner using '*' and spaces.
+ * Each character is represented in 7 lines with width 9.
+ * 
+ * @author Ashlesha
+ * @version 1.0
+ */
+public class UC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****   " + "  *****   " + " ******   " + "  *****   ");
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *     *  ");
+        System.out.println("*       * " + "*       * " + " *     *  " + "*         ");
+        System.out.println("*       * " + "*       * " + " ******   " + "  *****   ");
+        System.out.println("*       * " + "*       * " + " *        " + "       *  ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + " *     *  ");
+        System.out.println("  *****   " + "  *****   " + " *        " + "  *****   ");
+    }
+}
