760fc2424422ea292e81f61937dc18e5bfeb3e2d
Ashlesha Singh <ashleshasingh00@gmail.com>
2026-03-24 20:57:22 +0530

    UC1: Print OOPS

---
+++ UC1.java
@@ -0,0 +1,14 @@
+/**
+ * UC1 - Print OOPS to Console
+ * 
+ * This program prints "OOPS" to the console.
+ * 
+ * @author Ashlesha
+ * @version 1.0
+ */
+public class UC1 {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
